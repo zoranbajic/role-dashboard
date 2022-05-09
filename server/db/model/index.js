b6d960037835bdb5b905f6a0622d7315f4a10fc7
Zoran Bajic <zoranbajic@protonmail.com>
2022-05-08 22:10:52 -0500

    chore: create model associations

---
+++ server/db/model/index.js
@@ -0,0 +1,20 @@
+const Client = require('./client');
+const Contact = require('./client');
+const Role = require('./role');
+const Skill = require('./skill');
+
+Client.hasMany(Contact);
+Contact.belongsTo(Client);
+
+Contact.hasMany(Role);
+Role.belongsTo(Contact);
+
+Role.belongsToMany(Skill, { through: 'RoleSkills' });
+Skill.belongsToMany(Role, { through: 'RoleSkills' });
+
+module.exports = {
+  Client,
+  Contact,
+  Role,
+  Skill,
+};
